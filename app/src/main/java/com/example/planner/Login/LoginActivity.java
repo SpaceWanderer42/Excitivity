6740c684cd8dc0192f211a691c95e20224dac481
Octavian <jidovuoctav@gmail.com>
2021-04-19 18:18:06 +0300

    New commit

---
+++ app/src/main/java/com/example/planner/Login/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.planner.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+}
---
+++ app/src/main/java/com/example/planner/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.example.planner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.planner.Login.LoginActivity; // urmeaza a fi implementata
+
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }, 3000);
+
+
+
+    }
+
+}
