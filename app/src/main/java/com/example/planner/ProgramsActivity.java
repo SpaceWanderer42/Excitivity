f60aa0b4470ae0ff6e4880f96cfc434d5d61aad5
Octavian <jidovuoctav@gmail.com>
2021-05-14 17:47:14 +0300

    added new activities to + button (unfinished)

--- app/src/main/java/com/example/planner/ProgramsActivity.java
+++ app/src/main/java/com/example/planner/ProgramsActivity.java
@@ -8,8 +8,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class ProgramsActivity extends AppCompatActivity {
+
+    Button button1, button2, button3, button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +22,67 @@ public class ProgramsActivity extends AppCompatActivity {
 
         //getActionBar().setDisplayHomeAsUpEnabled(true);
 
+        button1 = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button3);
+        button3 = (Button) findViewById(R.id.button4);
+        button4 = (Button) findViewById(R.id.button5);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                openCooking();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                openSport();
+            }
+
+        });
+        button3.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                openStudy();
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                openPhotography();
+            }
+        });
 
     }
+    public void openCooking(){
+
+        Intent intent = new Intent (this, Cooking.class);
+        startActivity(intent);
+    }
+    public void openSport(){
+
+        Intent intent = new Intent (this, Sport.class);
+        startActivity(intent);
+    }
+    public void openStudy(){
+
+        Intent intent = new Intent (this, Study.class);
+        startActivity(intent);
+    }
+    public void openPhotography(){
+
+        Intent intent = new Intent (this, Photography.class);
+        startActivity(intent);
+    }
+
+
 /*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
