9a32a3c49d2233e64ca23e1015ac67fcfb10e436
andreea-rraluca <81642889+andreea-rraluca@users.noreply.github.com>
2021-05-15 20:50:00 +0300

    added icons, changed font for whole app

--- app/src/main/java/com/example/planner/Cooking.java
+++ app/src/main/java/com/example/planner/Cooking.java
@@ -1,33 +1,27 @@
 package com.example.planner;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 import java.util.Objects;
 
 public class Cooking extends AppCompatActivity implements View.OnClickListener {
-    public CardView cooking1;
-    public CardView cooking2;
-    public CardView cooking3;
-    public CardView cooking4;
-    public CardView cooking5;
-    public CardView cooking6;
-    public CardView cooking7;
-    public CardView cooking8;
-    public CardView cooking9;
-    public CardView cooking10;
-    public CardView cooking11;
-    public CardView cooking12;
-    public CardView cooking13;
-    public CardView cooking14;
+    public CardView card1;
+    public CardView card2;
+    public CardView card3;
+    public CardView card4;
+    public CardView card5;
+    public CardView card6;
+    public CardView card7;
 
+    public String baseString = "cooking";
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -41,21 +35,43 @@ public class Cooking extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cooking);
-        cooking1 = (CardView) findViewById(R.id.Cooking1);
-        cooking1.setOnClickListener(this);
-        cooking2 = (CardView) findViewById(R.id.Cooking2);
-        cooking2.setOnClickListener(this);
+        card1 = (CardView) findViewById(R.id.Cooking1);
+        card1.setOnClickListener(this);
+        card2 = (CardView) findViewById(R.id.Cooking2);
+        card2.setOnClickListener(this);
+        card3 = (CardView) findViewById(R.id.Cooking3);
+        card3.setOnClickListener(this);
+        card4 = (CardView) findViewById(R.id.Cooking4);
+        card4.setOnClickListener(this);
+        card5 = (CardView) findViewById(R.id.Cooking5);
+        card5.setOnClickListener(this);
+        card6 = (CardView) findViewById(R.id.Cooking6);
+        card6.setOnClickListener(this);
+        card7 = (CardView) findViewById(R.id.Cooking7);
+        card7.setOnClickListener(this);
         SharedPreferences datasaver = getSharedPreferences("tasks", MODE_PRIVATE);
         if(datasaver.getString("cooking1","").equals("1")) {
-            cooking1.setCardBackgroundColor(0xff328ca8);
+            card1.setCardBackgroundColor(0xff328ca8);
         }
         if(datasaver.getString("cooking2","").equals("1")) {
-            cooking2.setCardBackgroundColor(0xff328ca8);
+            card2.setCardBackgroundColor(0xff328ca8);
+        }
+        if(datasaver.getString("cooking3","").equals("1")) {
+            card3.setCardBackgroundColor(0xff328ca8);
+        }
+        if(datasaver.getString("cooking4","").equals("1")) {
+            card4.setCardBackgroundColor(0xff328ca8);
+        }
+        if(datasaver.getString("cooking5","").equals("1")) {
+            card5.setCardBackgroundColor(0xff328ca8);
+        }
+        if(datasaver.getString("cooking6","").equals("1")) {
+            card6.setCardBackgroundColor(0xff328ca8);
+        }
+        if(datasaver.getString("cooking7","").equals("1")) {
+            card7.setCardBackgroundColor(0xff328ca8);
         }
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-        /*<meta-data
-        android:name="android.support.PARENT_ACTIVITY"
-        android:value="MainActivity" />*/
 
     }
 
